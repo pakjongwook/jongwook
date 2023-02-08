c4ba671d62d29f2d1b148e286b9c76edafad53f2
박종욱\ejck <meso8587@naver.com>
2023-02-09 01:19:39 +0900

    문제 다풀음

--- 01_java/a002_operator/src/edu/kh/operator/practice/OperatorPractice4.java
+++ 01_java/a002_operator/src/edu/kh/operator/practice/OperatorPractice4.java
@@ -2,11 +2,33 @@ package edu.kh.operator.practice;
 
 import java.util.Scanner;
 
+
 public class OperatorPractice4 {
 	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		System.out.print("국어 : ");
+		int input1 = sc.nextInt();
+		
+		System.out.print("영어 : ");
+		int input2 = sc.nextInt();
+		
+		System.out.print("수학 : ");
+		int input3 = sc.nextInt();
+		
+		int sum1 = input1 + input2 + input3;
+		System.out.println("합계 : " + sum1);
+		
+		int averge = (double)sum1 /3; 
+		System.out.println("평균 : " +average);
+		String result = (input1 >= 40 && input2 >= 40) && input3 >= 40;
+		
+	
+		
+		
+		
+		
 		
 	}
 
