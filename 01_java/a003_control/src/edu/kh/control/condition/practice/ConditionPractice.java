becf30894eb46ae4207ac0d23294a66345eec5d2
park jong wook 9597 <meso8587@naver.com>
2023-02-09 17:53:31 +0900

    Switch 추가 , 연습문제

---
+++ 01_java/a003_control/src/edu/kh/control/condition/practice/ConditionPractice.java
@@ -0,0 +1,42 @@
+package edu.kh.control.condition.practice;
+
+import java.util.Scanner;
+
+// 실습문제 작성 클래스
+public class ConditionPractice {
+
+	public void practice1() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자를 한 개 입력하세요 :");
+		int input = sc.nextInt();
+		
+		String result;
+		if(input <= 0 ) {
+		 result = "양수만 입력해주세요";
+		} else if (input == 0) {
+			result = "짝수입니다";
+		} else {
+		result = "홀수입니다";
+		}
+		System.out.println(result);
+	}
+	
+	public void practice2() {
+		
+	}
+	
+	public void practice3() {
+		
+	}
+	
+	public void practice4() {
+		
+	}
+	
+	public void practice5() {
+		
+	}
+	
+	
+	
+}
