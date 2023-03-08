03d638d067aa5d9e3c2671b823b68de239b0cf18
park jong wook 9597 <meso8587@naver.com>
2023-03-08 15:58:38 +0900

    함수 문제 3월 8일

---
+++ 02_DB/함수 문제.sql
@@ -0,0 +1,41 @@
+-- 1번문제
+SELECT EMP_NAME "사원명" , ABS(FLOOR(HIRE_DATE - SYSDATE)) "근무일수1", -- 입사일-오늘 ABS(절대 값)
+ABS(TRUNC(SYSDATE-HIRE_DATE)) "근무일수2" -- 오늘-입사일
+FROM EMPLOYEE;
+
+-- 2번문제
+SELECT EMP_ID ,EMP_NAME ,EMAIL ,PHONE 
+FROM EMPLOYEE
+WHERE MOD(EMP_ID,2)=1; 
+-- 짝수 SELECT * FROM EMPLOYEE WHERE MOD(INDEX,2)=0;
+
+-- 3번 문제
+SELECT *
+FROM EMPLOYEE
+WHERE (MONTHS_BETWEEN(SYSDATE,HIRE_DATE) / 12) >=20; -- WHERE 사원이 어디에있는지 확인 
+-- MONTHS_BETWEEN(SYSDATE,HIRE_DATE) / 12 근무 N년차
+
+--4번 문제
+SELECT EMP_NAME, SUBSTR(EMP_NO , 1 , 7) || '******' 주민등록번호 -- 주민등록번호 맨 끝 '' 리터럴 표시 --> 8000000 '원' AS 별칭(단위)
+FROM EMPLOYEE;
+
+-- 5번문제
+SELECT EMP_NAME , JOB_CODE , NVL((SALARY+(SALARY*BONUS))*12, SALARY*12) "연봉(원)"  -- NVL(P1,P2) P1: NULL데이터를 처리할 컬럼명 혹은 값  그냥 연봉만
+																						--      P2: NULL 값을 대체하고자 하는 값 "()" --> 특수문자
+FROM EMPLOYEE;
+
+
+--6번
+
+SELECT EMP_ID ,EMP_NAME ,DEPT_CODE ,HIRE_DATE 
+FROM EMPLOYEE
+WHERE (DEPT_CODE = 'D5' OR DEPT_CODE = 'D9')
+AND EXTRACT(YEAR FROM HIRE_DATE) = 2004;
+
+-- 7번 
+SELECT EMP_NAME ,HIRE_DATE , LAST_DAY(HIRE_DATE) - HIRE_DATE +1 "입사한 달의 근무 일수" -- LAST_DAY 해당월의 마지막 입사일  - 입사한 날 EX) 내가 오늘 입사 8일 말까지 일하겠다 31일 => 31 -8 = 23(근무 일수)  
+FROM EMPLOYEE;
+
+-- 8번
+SELECT EMP_NAME ,DEPT_CODE ,TO_DATE(EMP_NO) 
+FROM EMPLOYEE;DD
