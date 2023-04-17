7ff5fd29edee933fd01b501c8a0982f4b37f0821
박종욱\ejck <meso8587@naver.com>
2023-04-17 11:55:54 +0900

    수업

---
+++ 03_jdbc/BoardProject/src/test1/Study3.java
@@ -0,0 +1,12 @@
+
+public class Study3 {
+	
+	public static void main(String[] args) {
+		string[] spell = {"p","o","s"};
+		string[] dic = {"sod","eocd","qixm","adio","soo"};
+		
+		System.out.print(study3(spell,dic));
+	}
+	
+
+}
