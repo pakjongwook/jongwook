fd1adeef2030664523d30f97f86392c1ebc627dd
pakjongwook <meso8587@naver.com>
2023-05-02 18:20:00 +0900

    semi 수정

--- 04_webFront/02_CSS/semi 프로젝트/main.js
+++ 04_webFront/02_CSS/semi 프로젝트/main.js
@@ -10,7 +10,7 @@ const modal = document.getElementById('modal');
 // nav 요소
 // const nav = document.querySelector('nav');
 
-// 열기 버튼을 누르면 nav의 display가 flex로 변경
+// 새 계정만들기 버튼을 누르면 display = flex로 보이기
 openBtn.addEventListener("click", () => {
     modal.style.display = "flex";
 });
