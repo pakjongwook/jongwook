9d7358e4bf9f3852f24ad20de1510a4ca90a46d6
pakjongwook <meso8587@naver.com>
2023-06-02 12:51:50 +0900

    no message

--- dev_finalProject - 복사본/js/summerNote/summberNote.js
+++ dev_finalProject - 복사본/js/summerNote/summberNote.js
@@ -1,8 +1,45 @@
-// 메인화면 페이지 로드 함수
 $(document).ready(function () {
-    $('#summernote').summernote({
-        placeholder: '내용을 작성하세요',
-        height: 400,
-        maxHeight: 400
-    });
+
+    var toolbar = [
+        // 글꼴 설정
+        ['fontname', ['fontname']],
+        // 글자 크기 설정
+        ['fontsize', ['fontsize']],
+        // 굵기, 기울임꼴, 밑줄,취소 선, 서식지우기
+        ['style', ['bold', 'italic', 'underline', 'strikethrough', 'clear']],
+        // 글자색
+        ['color', ['forecolor', 'color']],
+        // 글머리 기호, 번호매기기, 문단정렬
+        ['para', ['ul', 'ol', 'paragraph']],
+        // 줄간격
+        ['height', ['height']],
+        // 그림첨부, 링크만들기, 동영상첨부
+        ['insert', ['picture', 'link']],
+        // 코드보기, 확대해서보기, 도움말
+        ['view', ['codeview', 'fullscreen', 'help']]
+    ];
+
+    
+
+    var setting = {
+        height: 300,
+        minHeight: null,
+        maxHeight: null,
+        focus: true,
+        lang: 'ko-KR',
+        toolbar: toolbar,
+        callbacks: { //여기 부분이 이미지를 첨부하는 부분
+            onImageUpload: function (files, editor,
+                welEditable) {
+                for (var i = files.length - 1; i >= 0; i--) {
+                    uploadSummernoteImageFile(files[i],
+                        this);
+                }
+            }
+        }
+    };
+
+    $('#summernote').summernote(setting);
 });
+
+
